0515f1653fb21d97800ead2167565050c636c4f4
AraceliGS <aragutarras@gmail.com>
2018-03-22 14:46:05 -0500

    añadiendo nuevos componentes

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import NewOperation from './NewOperation'
+// import NewOperation from './NewOperation'
+import TransactionHistory from "./TransactionHistory";
+// import RextieBankAccounts from "./RextieBankAccounts";
 
 const Main = () => {
   return (
     <main className="col-12 col-sm-7 col-md-7 col-lg-7 col-xl-9">
-      <NewOperation icon="dinero (1).png"/>
+      {/* <NewOperation icon="dinero (1).png"/> */}
+      <TransactionHistory />
+      {/* <RextieBankAccounts /> */}
     </main>
   )
 }
