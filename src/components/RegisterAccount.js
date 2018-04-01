94684b2a3a97396239b45320200065d841afc9f5
EstrellaBF <stern.von.nord>
2018-04-01 13:23:59 -0500

    estilos al contenedor

--- src/components/RegisterAccount.js
+++ src/components/RegisterAccount.js
@@ -4,16 +4,25 @@ const RegisterAccount = () => (
   <div className="personal-bank-account">
     <div className="header-personal-bank-account">
       <h3>Cuentas Bancarias Personales -  </h3>
-      <img src="assets/icons/simbolo-mas-en-un-cuadrado-negro-redondeado.png"/>
+      <img src="assets/icons/simbolo-mas-en-un-cuadrado-negro-redondeado.png" />
     </div>
-    <div>
-      <p>Cuenta principal: </p> <span>0011-0117-95-0200296016</span>
-    </div>
-    <div>
-      <p>Cuenta secundaria: </p> <span>194-32408760-0-19</span>
-    </div>
-    <div>
-      <p>Cuenta secundaria: </p> <span>164-3065745960</span>
+    <div className="soles-account">
+      <p>Soles</p>
+      <table className="container-soles">
+        <tr>
+          <th>Banco</th>
+          <th>Tipo de Cuenta</th>
+          <th>Número de Cuenta</th>
+          <th className="in-use">En uso</th>
+        </tr>
+        <tr className="data-soles">
+          <td>BCP</td><i class="fas fa-star"></i>
+          <td>Ahorros</td>
+          <td>19136248273015</td>
+          <td className="in-use"><button type="button" className="active-button">Activo</button></td>
+          <td></td>
+        </tr>
+      </table>
     </div>
   </div>
 )
